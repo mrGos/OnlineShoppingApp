579fc2a9c3c43818cab56a074b8b8065d2615eca
mrGos <hoangkhang0235@gmail.com>
2019-01-01 09:25:41 +0700

    stable, error contact info

--- Common/UrlConfig.js
+++ Common/UrlConfig.js
@@ -1,4 +1,4 @@
-//const getUrl = () => 'http://10.20.85.101/shopdemo/api/';
+const getUrl = () => 'http://10.20.85.101/shopdemo/api/';
 //const getUrl = () => 'http://172.30.173.73/shopdemo/api/';
-const getUrl = () => 'http://10.211.55.3/shopdemo/api/'
+//const getUrl = () => 'http://10.211.55.3/shopdemo/api/'
 export default getUrl;
